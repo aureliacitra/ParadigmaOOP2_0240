16ef5fbc564e05942b3091c5cecdd03b4fde2e1b
aureliacitra <citrapangukir@gmail.com>
2025-06-02 18:32:38 +0700

    add class remotelampu dan membernya

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class remotelampu
+{
+    private:
+    string saklarNo[10];
+
+    public:
+    void setsaklarNo(int i, string value)
+    {
+      saklarNo[i] = value;
+    }
+    string getSaklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
